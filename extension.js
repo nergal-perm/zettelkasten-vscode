a24cddad232c18dc4f138468c75feb5babc1468f
ETerekhov <ETerekhov@aplana.com>
2017-10-03 17:59:50 +0500

    tweaking command

--- extension.js
+++ extension.js
@@ -15,7 +15,7 @@ function activate(context) {
     // The commandId parameter must match the command field in package.json
     var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
         // The code you place here will be executed every time your command is executed
-
+        
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
